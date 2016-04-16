5acab0f84ca408101d8ba801c124273773ad32d4
franzmoro <franzmoro88@gmail.com>
2016-04-16 15:31:57 +0100

    candidate view

--- frontend/src/js/views/Candidate/index.js
+++ frontend/src/js/views/Candidate/index.js
@@ -1,31 +1,23 @@
-import React from 'react'
-import { Grid, Row, Col } from 'react-bootstrap'
+import React, {Component} from 'react'
+import List from '../../components/List'
+import {candidates, roles} from '../../../../../database.js'
+import CandidateProfileModal from '../../components/CandidateProfile/CandidateProfileModal.js'
 
-const styles = {
-  textAlign: 'center',
-  img: {
-    outer: {
-      width: '80%',
-      maxWidth: '400px',
-      margin: '4em auto'
-    },
-    inner: {
-      width: '100%'
-    }
+export default class CandidateView extends Component {
+  constructor () {
+    super()
+    this.state = {roles, candidates, showModal: false}
+    this.changeState = this.changeState.bind(this)
   }
-}
-
-export default (props) => {
-  return (
-    <Grid style={styles}>
-      <Row>
-        <Col xs={12}>
-          <div style={styles.img.outer}>
-            <img style={styles.img.inner} src='img/rhino.png' />
-          </div>
-          <p>Your app goes here...</p>
-        </Col>
-      </Row>
-    </Grid>
-  )
+  changeState (state) {
+    this.setState(state)
+  }
+  render () {
+    return (
+      <div>
+        <List candidates={this.state.candidates} roles={this.state.candidates} changeState={this.changeState} />
+        <CandidateProfileModal />
+      </div>
+    )
+  }
 }
