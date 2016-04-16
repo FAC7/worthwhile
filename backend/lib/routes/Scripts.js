d1671ef6803615e6deb801c5e18e7e5561ef3272
franzmoro <franzmoro88@gmail.com>
2016-04-16 19:55:42 +0100

    fixed server error and added correct postgres backup file

--- backend/lib/routes/Scripts.js
+++ backend/lib/routes/Scripts.js
@@ -2,7 +2,7 @@ export default {
   path: '/{filename}.js',
   method: 'GET',
   handler: (request, reply) => {
-    const js = './public' + response.path
+    const js = './public' + request.path
     reply.file(js)
   }
 }
