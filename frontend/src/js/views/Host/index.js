c1bff2a1b13f76a44e7b73a837f263847499f4d3
franzmoro <franzmoro88@gmail.com>
2016-04-17 10:48:03 +0100

    component did mount ajax calls on host view

--- frontend/src/js/views/Host/index.js
+++ frontend/src/js/views/Host/index.js
@@ -3,11 +3,24 @@ import CollapsibleItem from '../../components/CollapsibleItem/index.js'
 import {candidates} from '../../../../../database.js'
 import CandidateProfileModal from '../../components/CandidateProfile/CandidateProfileModal.js'
 
+const loggedInRoleID = '2'
+
 export default class HostView extends Component {
   constructor () {
     super()
-    this.state = {candidates, showModal: false, currCandidate: null}
+    this.state = {candidates, showModal: false, currCandidate: null, loggedInRoleID}
     this.changeState = this.changeState.bind(this)
+  }
+  componentDidMount () {
+    let xhr = new XMLHttpRequest() // eslint-disable-line
+    xhr.onreadystatechange = () => {
+      console.log('sending request')
+      if (xhr.status === 200 && xhr.readyState === 4) {
+        console.log('here\'s the response text!', JSON.parse(xhr.responseText))
+      }
+    }
+    xhr.open('GET', `/getCandidatesByRole/${this.state.currentRole}`)
+    xhr.send()
   }
   changeState (state) {
     this.setState(state)
