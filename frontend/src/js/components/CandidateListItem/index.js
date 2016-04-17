684c3811970cf20cd42f8802abf81a3bca4ef1c1
Owen TM <oturnermajor@gmail.com>
2016-04-17 14:42:16 +0100

    updating state

--- frontend/src/js/components/CandidateListItem/index.js
+++ frontend/src/js/components/CandidateListItem/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Button} from 'react-bootstrap'
+import {Button, MenuItem, DropdownButton} from 'react-bootstrap'
 
 export default (props) => {
   const seeMore = () => {
@@ -7,9 +7,51 @@ export default (props) => {
       showModal: true
     })
   }
+
+  const updateCandidateProgress = (roleID, candidateID, status) => {
+    const xhr = new XMLHttpRequest() // eslint-disable-line
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        console.log('Updated progress successfully!')
+      }
+    }
+    xhr.open('GET', `/updateCandidateProgress/${roleID}/${candidateID}/${status}`)
+    xhr.send()
+
+    const thisCandidate = {
+      ...props.candidate,
+      status
+    }
+    const oldState = props.getState()
+    const filteredCandidates = oldState.candidates.filter((candidate) => {
+      candidate.uuid !== candidateID
+    })
+    const newCandidates = [...filteredCandidates, thisCandidate]
+
+    const newState = {
+      ...oldState,
+      candidates: newCandidates
+    }
+    this.changeState(newState)
+  }
   return (
     <li style={props.liStyle} key={props.candidate.candidateUUID}>
-      {props.candidate.name} <Button bsStyle='primary' onClick={seeMore}>SEE MORE</Button>
+      {props.candidate.name}
+      <Button bsStyle='primary' onClick={seeMore}>Details</Button>
+      <DropdownButton title={props.candidate.status} id='dropdown-size-large'>
+        <MenuItem
+          eventKey='2'
+          onClick={() => { updateCandidateProgress(props.roleID, props.candidate.candidateUUID, 'applied') }}
+          >Applied</MenuItem>
+        <MenuItem
+          eventKey='1'
+          onClick={() => { updateCandidateProgress(props.roleID, props.candidate.candidateUUID, 'interviewed') }}
+          >Interviewed</MenuItem>
+        <MenuItem
+          eventKey='2'
+          onClick={() => { updateCandidateProgress(props.roleID, props.candidate.candidateUUID, 'accepted') }}
+          >Accepted</MenuItem>
+      </DropdownButton>
     </li>
   )
 }
