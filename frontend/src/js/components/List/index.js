9607b73ee396b2817911e18e9e9a1e7cd10cfe63
Mireia <mireia@mireiasangalo.com>
2016-04-17 11:02:12 +0100

    change style for background

--- frontend/src/js/components/List/index.js
+++ frontend/src/js/components/List/index.js
@@ -1,24 +1,29 @@
 import React from 'react'
 import HostListItem from '../HostListItem'
 import CandidateListItem from '../CandidateListItem'
+import { Button } from 'react-bootstrap'
+
+
 
 export default (props) => {
   return (
-      <ul style={ulStyle}>
+    <li style={this.props.style}>
       {props.roles
         ? props.roles.map(role => <HostListItem role={role} changeState={props.changeState} liStyle={liStyle}/>)
         .filter(props.filterFunction)
         : props.candidates.map(candidate => <CandidateListItem candidate={candidate} changeState={props.changeState} liStyle={liStyle} />)
         .filter(props.filterFunction)
       }
-      </ul>
+    </li>
   )
 }
 
 const ulStyle = {
   backgroundColor: '#03A9F4',
   padding: '2em',
-  borderRadius: '10px'
+  borderRadius: '10px',
+  color: 'white',
+  position: 'inherit'
 }
 
 const liStyle = {
